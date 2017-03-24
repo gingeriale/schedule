d4771868bdba43679586e4935503fd228479c686
Margarita Yanochkina <yanochkina.mv@gmail.com>
2017-03-25 00:26:33 +0300

    time format fix

--- src/common/util/formatDateTime.js
+++ src/common/util/formatDateTime.js
@@ -4,7 +4,7 @@ import ru from 'date-fns/locale/ru'
 const formatDateTime = (schDetails) => {
     Object.values(schDetails).forEach(lect => {
         lect.date = format(lect.date, 'DD MMMM YYYY', {locale: ru})
-        lect.time = format(lect.time, 'HH:MM', {locale: ru})
+        lect.time = format(lect.time, 'HH:mm', {locale: ru})
     })
     return schDetails
 }
